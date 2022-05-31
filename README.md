1f896aa1609178172549e840e61a36758f5b2ee3
Cristian Quesada Cossio <crisquesadaco@gmail.com>
2022-05-31 06:16:51 -0500

    Usando List comprehension

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #Ejercicios y talleres de Misión Tic 2022 UTP
 
 Solución a los ejercicios que propuso el formador Cesar Díaz
+
---
+++ Unidad 4/list_comprehension.py
@@ -0,0 +1,7 @@
+#List comprehension: Sirve para crear estructuras a partir de otras estructuras.
+#Lista: [<dato> <recorrido for> <filtro if>]
+
+#Generar una nueva lista con las frutas que tengan la letra a.
+frutas = ['manzana', 'pera', 'kiwi', 'mango', 'melon', 'sandia', 'naranja', 'platano', 'fresa', 'uva', 'papaya', 'limon']
+lista = [x for x in frutas if "a" in x]
+print(lista)
