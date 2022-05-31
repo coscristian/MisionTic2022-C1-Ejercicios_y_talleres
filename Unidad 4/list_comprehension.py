ac3411d5f1b2402b1dec054cf14ce717bc342053
Cristian Quesada Cossio <crisquesadaco@gmail.com>
2022-05-31 12:28:34 -0500

    Correccion de errores

--- Unidad 4/list_comprehension.py
+++ Unidad 4/list_comprehension.py
@@ -4,4 +4,3 @@
 #Generar una nueva lista con las frutas que tengan la letra a.
 frutas = ['manzana', 'pera', 'kiwi', 'mango', 'melon', 'sandia', 'naranja', 'platano', 'fresa', 'uva', 'papaya', 'limon']
 lista = [x for x in frutas if "a" in x]
-print(lista)
